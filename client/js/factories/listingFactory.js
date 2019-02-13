fabbea5ae862b62922efce725ed521bd9ceb18cc
yiflin <yifanlin1998@gmail.com>
2019-02-12 22:31:54 -0500

    Update listingFactory.js

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,18 +1,18 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('http:///api/listings');
+      return $http.get('/api/listings');
     },
 	
 	create: function(listing) {
-	  return $http.post('http:///api/listings', listing);
+	  return $http.post('/api/listings', listing);
     }, 
 
     delete: function(id) {
 	   /**TODO
         return result of HTTP delete method
        */
-	  return $http.delete('http:///api/listings/' + id);
+	  return $http.delete('/api/listings/' + id);
     }
   };
 
