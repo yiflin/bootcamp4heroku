34623361353183ef5345b388e02b7d6fd68f53b6
yiflin <yifanlin1998@gmail.com>
2019-02-12 23:09:13 -0500

    Update app.js

--- server/config/app.js
+++ server/config/app.js
@@ -4,7 +4,7 @@ var config = require('./config'),
 
 module.exports.start = function() {
   var app = express.init();
-  app.listen(process.env.port || 8080, function() {
+  app.listen(process.env.PORT || 8080, function() {
     console.log('App listening on port', this.address().port, app.settings.env);
   });
 };
